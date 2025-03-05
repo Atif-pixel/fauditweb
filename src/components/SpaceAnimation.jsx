d0b3fb7b7e402df6cd56dbba3806de7bb187c35f
jyoti-4735 <jyotikambar145@gmail.com>
2025-03-06 03:59:09 +0530

    updated about  page

--- src/components/SpaceAnimation.jsx
+++ src/components/SpaceAnimation.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import starsImage from '../assets/Star.png'; // Import the stars image
-import earthImage from '../assets/Earth.png'; // Import the Earth image
+import auditImage from '../assets/Audit.png'; // Import the Audit image
+import moneyImage from '../assets/Money.png'; // Import the Money image
 import './SpaceAnimation.css';
 
 const SpaceAnimation = () => {
   return (
     <div className="space-container">
-      <div className="stars" style={{ backgroundImage: `url(${starsImage})` }}></div>
-      <img src={earthImage} alt="Earth" className="earth" />
+      <div className="audit" style={{ backgroundImage: `url(${auditImage})` }}></div>
+      <img src={moneyImage} alt="Money" className="money" />
     </div>
   );
 };
